4c0017a5139fd565eb5a95c4390ab36c0350ac86
i-zanis <ioanlafazanis@gmail.com>
2021-02-27 23:41:44 +0000

    update MessagesSender.js

--- src/MessageSender.js
+++ src/MessageSender.js
@@ -2,14 +2,27 @@ import React from "react";
 import "./SidebarRow.css";
 import { Avatar } from "@material-ui/core";
 
-function SidebarRow({ src, Icon, title }) {
-    return (
-        <div className="sidebarRow">
-            {src && <Avatar src={src} />}
-            {Icon && <Icon />}
-            <h4>{title}</h4>
+function MessageSender() { src, Icon, title }) {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+    return ( 
+        <div className="messageSender">
+            <div className="messageSender__top">
+                <Avatar />
+                <div className="messageSender__top">
+                    <input
+                        className="messageSender_input"
+                        placeholder={"What's on your mind"}
+                    />
+                    <input placeholder="image URL (Optional)" />
+                    <button onClick={handSubmit} type="submit">
+                        Hidden submit
+                    </form>
+            </div>
+            <div className="messageSender_bottom"></div>
         </div>
-    )
+    );
 }
 
-export default App;
+export default MessagerSender;
